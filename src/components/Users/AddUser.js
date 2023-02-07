7781ef1f1a66a118c2187acac7b25380a41962ea
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-07 15:03:07 +0300

    Returned the foem markup

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,17 +1,21 @@
 import React from "react";
 
-const AddUser = (props) => {
-  const addUserHandler = (e) => {
+const AddUser = (props) =>
+{
+  const addUserHandler = (e) =>
+  {
     e.preventDefault();
-  };
+  }
 
-  <form action="" onSubmit={addUserHandler}>
-    <label htmlFor="username">Username</label>
-    <input type="text" name="Username" id="username" />
-    <label htmlFor="age">Age (Years)</label>
-    <input type="number" name="userage" id="age" />
-    <button type="submit">Add User</button>
-  </form>;
+  return (
+    <form action="" onSubmit={addUserHandler}>
+      <label htmlFor="username">Username</label>
+      <input type="text" name="Username" id="username" />
+      <label htmlFor="age">Age (Years)</label>
+      <input type="number" name="userage" id="age" />
+      <button type="submit">Add User</button>
+    </form>
+  )
 };
 
 export default AddUser;
