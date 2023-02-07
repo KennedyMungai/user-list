98fee9fd46ab2c28767dc479af88bfbd10499f62
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-07 15:32:18 +0300

    Created a dynamic function call for the onClick event

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -4,7 +4,13 @@ import classes from './Button.module.css'
 const Button = (props) =>
 {
     return (
-        <button className={classes.button} type={props.type || 'button'}>Add User</button>
+        <button
+            className={classes.button}
+            type={props.type || 'button'}
+            onClick={props.onClick}
+        >
+            Add User
+        </button>
     )
 }
 
