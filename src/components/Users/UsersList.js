74c4dedfdd2cbfb1a709fe2ff4eabd15d6d8b95e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-08 09:25:49 +0300

    Solved a simple syntax error

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -5,10 +5,11 @@ const UsersList = (props) =>
     return (
         <ul>
             {props.users.map((user) =>
+            (
                 <li>
                     {user.name} ({user.age} years old)
                 </li>
-            },
+            ))}
         </ul>
     )
 }
