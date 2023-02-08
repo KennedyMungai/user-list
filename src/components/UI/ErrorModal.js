e4c6bd09b1a5a55dd9bfb3ce326daba1f3994bde
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-08 10:03:14 +0300

    Added some markup elements and and import for the Button component

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import Button from './Button'
 import Card from './Card'
 
 const ErrorModal = (props) =>
 {
     return (
         <Card>
+            <header>
+                <h2></h2>
+            </header>
+            <div>
+                <p></p>
+            </div>
+            <footer>
 
+            </footer>
         </Card>
     )
 }
